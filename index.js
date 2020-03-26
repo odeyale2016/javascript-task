e7f5720024333970c78a20c41e79bc8b80fa20fa
odeyale kehinde <odeyale_kehinde@yahoo.com>
2020-03-26 15:20:22 +0100

    javascript task

---
+++ index.js
@@ -0,0 +1,20 @@
+var name="Odeyale Kehinde";
+var courses=['PHP','Javascricpt','HTML','Nodejs','Python'];
+var x;
+console.log(name);
+console.log(courses);
+if(courses.length%2===1){
+   var x=1;
+   while(x<=200){
+       console.log(x);
+       x+=2;
+   }
+   }else if(courses.length%2===0){
+    var x=2;
+    while(x<=200){
+        console.log(x);
+        x+=2;
+    }
+   }
+
+
